92d85de52c1f85486eb0713e44283c9ba8dd7d24
J Lomako <jlomako@web.de>
2023-03-24 09:42:42 -0400

    add random button, function random_figure, session.state counter

--- main.py
+++ main.py
@@ -4,23 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import io
 
-
 @st.cache_data(ttl=3600) # clear cache after 1h
-def create_figure(R, r, d, freq1, freq2, amp1, amp2, k, number):
+def create_figure(R, r, d, freq1, freq2, amp1, amp2, k, seed):
     fig, ax = plt.subplots(figsize=(10, 10))
 
     R = R # big circle radius
     r = r # # small circle radius
-    d = d # r * 1.5  # distance from the center of the small circle to the tracing point
+    d = d # distance from the center of the small circle to the tracing point
     freq1 = freq1  # frequency of first sine wave
     freq2 = freq2  # frequency of second sine wave
     amp1 = amp1  # amplitude of first sine wave
     amp2 = amp2  # amplitude of second sine wave
     k = np.pi / k  # coefficient to adjust the shape of the pattern
-
     theta = np.linspace(0, 60 * np.pi, 10000)
-
-    # print(f"# R: {R}, r: {r}, freq1: {freq1}, freq2: {freq2}, amp1: {amp1}, amp2: {amp2}, k: {k}")
 
     # add sine and cosine waves to x and y equations
     x = (R - r) * np.cos(theta) + d * np.cos((R - r) / r * theta) + k * amp1 * np.sin(
@@ -34,13 +30,38 @@ def create_figure(R, r, d, freq1, freq2, amp1, amp2, k, number):
     ax.axis('off')
     return fig
 
+@st.cache_data
+def random_figure(seed):
+    return [np.random.randint(1, 200),  # R
+         np.random.randint(1, 200), # r
+         np.random.randint(1, 200), # d
+         np.random.randint(1, 10),  # freq1
+         np.random.randint(1, 10),  # freq2
+         np.random.randint(1, 500), # amp1
+         np.random.randint(1, 500), # amp2
+         np.random.randint(1, 20)]  # k
+
+
+
 st.subheader("Spirograph: Generate geometric figures")
 
+# counter for random_figure
+if 'count' not in st.session_state:
+    st.session_state.count = 0
 
-col1, col2 = st.columns([1, 4])
+if st.session_state.count == 0:
+    spiro = [1, 71, 192, 4, 4, 100, 74, 4]
+else:
+    spiro = random_figure(st.session_state.count)
 
-spiro = [1,71,192,4,4,100,74,4]
 
+random_button = st.button("Random")
+if random_button:
+  st.session_state.count += 1
+  spiro = random_figure(st.session_state.count)
+
+
+col1, col2 = st.columns([1, 5])
 with col1:
     R = st.slider('R: ', 1, 200, value=spiro[0])
     r = st.slider('r: ', 1, 200, value=spiro[1])
@@ -49,7 +70,7 @@ with col1:
     freq2 = st.slider('freq2: ', 1, 10, value=spiro[4])
     amp1 = st.slider('amp1: ', 1, 500, value=spiro[5])
     amp2 = st.slider('amp2: ', 1, 500, value=spiro[6])
-    k = st.slider('k: ', 1, 20, value=spiro[7])
+    k = st.slider('k: ', 1, 20, value=spiro[7], key="k")
 
 with col2:
     st.pyplot(create_figure(R, r, d, freq1, freq2, amp1, amp2, k, np.random.random()))
@@ -64,4 +85,5 @@ with col2:
         file_name=fn,
         mime="image/png"
     )
+
 
