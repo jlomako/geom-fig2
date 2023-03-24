5d804fde8b1761216b2d994ec014a94b1fdd89a6
J Lomako <jlomako@web.de>
2023-03-24 12:00:47 -0400

    change some values in sliders, some layout changes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import io
 
+st.set_page_config(page_title="Spirograph", layout="centered")
+
 @st.cache_data(ttl=3600) # clear cache after 1h
 def create_figure(R, r, d, freq1, freq2, amp1, amp2, k, seed):
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -18,7 +20,7 @@ def create_figure(R, r, d, freq1, freq2, amp1, amp2, k, seed):
     k = np.pi / k  # coefficient to adjust the shape of the pattern
     theta = np.linspace(0, 60 * np.pi, 10000)
 
-    # add sine and cosine waves to x and y equations
+    # x and y equations with sine and cosine waves
     x = (R - r) * np.cos(theta) + d * np.cos((R - r) / r * theta) + k * amp1 * np.sin(
         freq1 * theta) + k * amp2 * np.cos(freq2 * theta)
     y = (R - r) * np.sin(theta) - d * np.sin((R - r) / r * theta) + k * amp1 * np.cos(
@@ -30,60 +32,63 @@ def create_figure(R, r, d, freq1, freq2, amp1, amp2, k, seed):
     ax.axis('off')
     return fig
 
-@st.cache_data
+
+@st.cache_data(ttl=3600)
 def random_figure(seed):
     return [np.random.randint(1, 200),  # R
-         np.random.randint(1, 200), # r
-         np.random.randint(1, 200), # d
-         np.random.randint(1, 10),  # freq1
-         np.random.randint(1, 10),  # freq2
-         np.random.randint(1, 500), # amp1
-         np.random.randint(1, 500), # amp2
-         np.random.randint(1, 20)]  # k
-
+            np.random.randint(1, 200), # r
+            np.random.randint(1, 200), # d
+            np.random.randint(1, 10),  # freq1
+            np.random.randint(1, 10),  # freq2
+            np.random.randint(1, 500), # amp1
+            np.random.randint(1, 500), # amp2
+            np.random.randint(1, 20)]  # k
 
 
 st.subheader("Spirograph: Generate geometric figures")
 
-# counter for random_figure
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-
-if st.session_state.count == 0:
-    spiro = [1, 71, 192, 4, 4, 100, 74, 4]
-else:
-    spiro = random_figure(st.session_state.count)
-
-
-random_button = st.button("Random")
-if random_button:
-  st.session_state.count += 1
-  spiro = random_figure(st.session_state.count)
-
-
-col1, col2 = st.columns([1, 5])
+col1, col2 = st.columns([2, 6])
 with col1:
-    R = st.slider('R: ', 1, 200, value=spiro[0])
-    r = st.slider('r: ', 1, 200, value=spiro[1])
-    d = st.slider('d: ', 1, 200, value=spiro[2])
-    freq1 = st.slider('freq1: ', 1, 10, value=spiro[3])
-    freq2 = st.slider('freq2: ', 1, 10, value=spiro[4])
-    amp1 = st.slider('amp1: ', 1, 500, value=spiro[5])
-    amp2 = st.slider('amp2: ', 1, 500, value=spiro[6])
-    k = st.slider('k: ', 1, 20, value=spiro[7], key="k")
+    # counter for random_figure
+    if 'count' not in st.session_state:
+        st.session_state.count = 0
+
+    # use default figure on first run otherwise use fig(seed) from random_button
+    if st.session_state.count == 0:
+        spiro = [1, 71, 192, 4, 4, 100, 74, 4]
+    else:
+        spiro = random_figure(st.session_state.count)
+
+    # set seed to create different figures
+    # seed needs to be random and can not be counter!
+    # counter would create the same figures all over again
+    random_button = st.button("Create", use_container_width=True)
+    if random_button:
+        st.session_state.count = np.random.random()
+        spiro = random_figure(st.session_state.count)
+
+
+    R = st.slider('R: ', 0, 200, value=spiro[0], label_visibility="collapsed")
+    r = st.slider('r: ', 1, 200, value=spiro[1], label_visibility="collapsed")
+    d = st.slider('d: ', 0, 200, value=spiro[2], label_visibility="collapsed")
+    freq1 = st.slider('freq1: ', 0, 10, value=spiro[3], label_visibility="collapsed")
+    freq2 = st.slider('freq2: ', 0, 10, value=spiro[4], label_visibility="collapsed")
+    amp1 = st.slider('amp1: ', 0, 500, value=spiro[5], label_visibility="collapsed")
+    amp2 = st.slider('amp2: ', 0, 500, value=spiro[6], label_visibility="collapsed")
+    k = st.slider('k: ', 1, 25, value=spiro[7], label_visibility="collapsed")
 
 with col2:
     st.pyplot(create_figure(R, r, d, freq1, freq2, amp1, amp2, k, np.random.random()))
 
-    fn = 'image.png'
     img = io.BytesIO()
     plt.savefig(img, format='png')
 
     st.download_button(
-        label="Save",
+        label="Download image",
         data=img,
-        file_name=fn,
-        mime="image/png"
+        file_name='image.png',
+        mime="image/png",
+        use_container_width=True
     )
 
 
